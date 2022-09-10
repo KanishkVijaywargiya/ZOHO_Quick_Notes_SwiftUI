1262a5a620d64be59fb0e8bad52f4650402fc883
KanishkVijaywargiya <secretcodersociety@gmail.com>
2022-09-10 11:27:56 +0530

    Initial Commit

---
+++ QuickNote/QuickNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNoteApp.swift
+//  QuickNote
+//
+//  Created by Kanishk Vijaywargiya on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
